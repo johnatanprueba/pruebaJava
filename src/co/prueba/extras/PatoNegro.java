3a8d189284cb3e3394a192a7ef79c900ec7d3dbf
Johnatan Naranjo Acevedo <johnatannaranjo.fcv@gmail.com>
2026-07-01 21:28:13 -0500

    Agregamos pato negro

---
+++ src/co/prueba/extras/PatoNegro.java
@@ -0,0 +1,37 @@
+package co.prueba.extras;
+
+enum LETRAS {
+	A, B {
+		public void saludar() {
+		}
+	}
+}
+
+class Sup{
+	class G{
+		
+	}
+	
+	static class Est{}
+	
+	public void saludar() {
+		
+		Sup.G g = new Sup.G();
+		G g2=new G();
+		
+		Est est = new Sup.Est();
+		Est est2 = new Est();
+	}
+}
+
+public class PatoNegro {
+	public static void main(String...args) {
+		LETRAS l = LETRAS.B;
+		
+		var f = """
+				              j
+				              """;
+		
+		System.out.println(f);
+	}
+}
